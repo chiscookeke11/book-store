900019e1022403685f500bfbedcdbbcf713b4dc4
chiscookeke11 <chiscookeke11@gmail.com>
2024-12-22 11:17:24 -1100

    ADD: tracks

--- book-store/book-store/pages/HomePage.jsx
+++ book-store/book-store/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/herosection/Hero'
 import PointBanner from '../components/pointsbanner/PointBanner'
+import Tracks from '../components/Tracks'
 
 
 const HomePage = () => {
@@ -10,6 +11,7 @@ const HomePage = () => {
     }} >
         <Hero/>
         <PointBanner/>
+        <Tracks/>
     </div>
   )
 }
--- book-store/book-store/tailwind.config.js
+++ book-store/book-store/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: '0px 4px 25px 0px rgba(0, 0, 0, 0.1)', 
+      },
+    },
   },
   plugins: [],
 };
